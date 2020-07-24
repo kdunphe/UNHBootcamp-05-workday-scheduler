733a68422b71d20888707fcd941cd2b45c4eecf9
kdunphe <57336712+kdunphe@users.noreply.github.com>
2020-07-23 21:13:56 -0400

    Updating files.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,13 @@
 $(document).ready()
+
+
+// moment().format('MMMM Do YYYY, h:mm:ss a');
+// console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
+// moment().endOf('day').fromNow(); 
+
+
+function currentDay() {
+    $("#currentDay").html([moment().format('dddd, ' + 'MMMM Do YYYY')]);
+}
+
+$(currentDay);
